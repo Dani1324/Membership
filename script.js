067776f7c12a279e0641f033292cb1432be13d31
Dani1324 <danieleronchini2@gmail.com>
2023-02-09 23:17:29 +0100

    3

--- script.js
+++ script.js
@@ -40,72 +40,59 @@ gsap.to('.char', {
 sliderload. from(".fade-in", {
   y: 0, 
   delay: 0.3,
-  duration: 4,
+  duration: 3,
   scale: 1,
   ease: "Expo.easeOut",
 })
 
+// GSAP - image-slider pin
+gsap. to(".image-slider", {
+  delay: 0.3,
+  duration: 7,
+  scrollTrigger: {
+    trigger: ".image-slider",
+    start: "90% 80%",
+    end: "90% 0%",
+    pin: true,
+    toggleActions: "none none none none",
+  },
+})
+
+// GSAP - image-slider
+const t1 = gsap.timeline({
+  ease: Power1.easeOut,
+  scrollTrigger: {
+    trigger: ".image-1, image-2, image-3",
+    start: "105% 80%",
+    end: "80% 0%",
+    scrub: 4,
+    toggleActions: "none none none none",
+  },
+});
+
+const t2 = gsap.timeline({
+  ease: Power1.easeOut,
+  scrollTrigger: {
+    trigger: ".image-1, image-2, image-3",
+    start: "105% 80%",
+    end: "46% 0%",
+    scrub: 4,
+    toggleActions: "none none none none",
+  },
+});
+
+const t3 = gsap.timeline({
+  ease: Power1.easeOut,
+  scrollTrigger: {
+    trigger: ".image-1, image-2, image-3",
+    start: "105% 80%",
+    end: "46% 0%",
+    scrub: 4,
+    toggleActions: "none none none none",
+  },
+});
+
 sliderload.eventCallback("onComplete", function() {
-
-  // GSAP - image-slider pin
-  gsap. to(".image-slider", {
-    delay: 0.3,
-    duration: 7,
-    scrollTrigger: {
-      trigger: ".image-slider",
-      start: "90% 80%",
-      end: "90% 0%",
-      pin: true,
-      toggleActions: "none none none none",
-    },
-  })
-
-  // GSAP - image-slider
-  const t1 = gsap.timeline({
-    paused: true,
-    ease: Power1.easeOut,
-    scrollTrigger: {
-      trigger: ".image-1, image-2, image-3",
-      start: "105% 80%",
-      end: "80% 0%",
-      scrub: 4,
-      toggleActions: "none none none none",
-      markers: {
-        startColor: "purple",
-        endColor: "fuchsia",
-        fontSize: "1rem",
-      },
-    },
-  });
-
-  const t2 = gsap.timeline({
-    paused: true,
-    ease: Power1.easeOut,
-    scrollTrigger: {
-      trigger: ".image-1, image-2, image-3",
-      start: "105% 80%",
-      end: "46% 0%",
-      scrub: 4,
-      toggleActions: "none none none none",
-      markers: {
-        startColor: "purple",
-        endColor: "fuchsia",
-        fontSize: "1rem",
-      },
-    },
-  });
-
-  const t3 = gsap.timeline({
-    paused: true,
-    ease: Power1.easeOut,
-    scrollTrigger: {
-      trigger: ".image-1, image-2, image-3",
-      start: "105% 80%",
-      end: "46% 0%",
-      scrub: 4,
-      toggleActions: "none none none none",
-    },
-  });
 
   t1. to(".image-1", {
     x: '-100vw',
