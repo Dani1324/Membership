0cdc4a50eb37e3e0dc3e5ed156330ecba803f8b9
Dani1324 <danieleronchini2@gmail.com>
2023-02-10 16:31:32 +0100

    5

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 
-// Add smooth scrolling to all links
-$("a#ciao").on('click', function(event) {
-
-  // Make sure this.hash has a value before overriding default behavior
+// Smooth scrolling to all .smooth links
+$("a.smooth").on('click', function(event) {
   if (this.hash !== "") {
-      // Prevent default anchor click behavior
     event.preventDefault();
-
-    // Store hash
     var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 800, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
       window.location.hash = hash;
     });
   }
