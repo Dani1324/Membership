b945115d3e47604ba928dbe2ac71fd7886043728
Dani1324 <danieleronchini2@gmail.com>
2023-02-11 22:08:04 +0100

    6

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-// Smooth scrolling to all .smooth links
+// smooth scrolling to all .smooth links
 $("a.smooth").on('click', function(event) {
   if (this.hash !== "") {
     event.preventDefault();
@@ -14,106 +13,59 @@ $("a.smooth").on('click', function(event) {
 
 
 // GSAP - settings
-const myText = new SplitType('.line');
-const sliderload = gsap.timeline({});
 gsap.registerPlugin(ScrollTrigger);
+const myText = new SplitType('.line');
+const imageSliderPin = gsap.timeline();
+
 
 // GSAP - text animation
-gsap.to('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: .6,
-})
+gsap. to('.char', {
+  y: 0,
+  stagger: 0.05,
+  delay: 0.2,
+  duration: .6,
+});
 
-// GSAP - slider loading
-sliderload. from(".fade-in", {
-  y: 0, 
-  delay: 0.3,
+// GSAP - image slider - pin
+imageSliderPin. to(".image-slider", {
+  delay: 0.15,
   duration: 3,
-  scale: 1,
-  ease: "Expo.easeOut",
-})
-
-// GSAP - image-slider pin
-gsap. to(".image-slider", {
-  delay: 0.3,
-  duration: 7,
   scrollTrigger: {
     trigger: ".image-slider",
-    start: "90% 80%",
+    start: "95% 90%",
     end: "90% 0%",
     pin: true,
-    toggleActions: "none none none none",
+    toggleActions: "repeat none none none",
+    markers: false,
   },
 })
 
-// GSAP - image-slider
-const t1 = gsap.timeline({
-  ease: Power1.easeOut,
-  scrollTrigger: {
-    trigger: ".image-1, image-2, image-3",
-    start: "105% 80%",
-    end: "80% 0%",
-    scrub: 4,
-    toggleActions: "none none none none",
-  },
-});
+imageSliderPin.eventCallback("onComplete", function() {
 
-const t2 = gsap.timeline({
-  ease: Power1.easeOut,
-  scrollTrigger: {
-    trigger: ".image-1, image-2, image-3",
-    start: "105% 80%",
-    end: "46% 0%",
-    scrub: 4,
-    toggleActions: "none none none none",
-  },
-});
-
-const t3 = gsap.timeline({
-  ease: Power1.easeOut,
-  scrollTrigger: {
-    trigger: ".image-1, image-2, image-3",
-    start: "105% 80%",
-    end: "46% 0%",
-    scrub: 4,
-    toggleActions: "none none none none",
-  },
-});
-
-sliderload.eventCallback("onComplete", function() {
-
-  t1. to(".image-1", {
-    x: '-100vw',
+  // GSAP - image-slider - animation
+  const t1 = gsap.timeline({
+    ease: Power4.easeOut,
+    scrollTrigger: {
+      trigger: ".image-1, image-2, image-3",
+      start: "90% 95%",
+      end: "90% 0%",
+      scrub: 4,
+      toggleActions: "repeat none none none",
+      markers: false,
+    },
   });
 
-  t1. to(".image-2", {
-    x: '-100vw',
+  t1.to(".image-1", {
+    delay: 0.3,
+    y: "-0.5vw",
+    opacity: 0,
+    scale: 1.05,
   });
 
-  t2. to(".image-2", {
-    scale: 1,
-    y: '5vw',
-  });
-
-  t3. to(".image-3", {
-    scale: 0.95,
-    y: '2.5vw',
-  });
-
-  t3. to(".image-3", {
-    scale: 1,
-    y: '5vw',
+  t1.to(".image-2", {
+    y: "-0.5vw",
+    opacity: 0,
+    scale: 1.05,
   });
 
 });
-
-
-/*
-  markers: {
-          startColor: "purple",
-          endColor: "fuchsia",
-          fontSize: "1rem",
-        },
-*/
