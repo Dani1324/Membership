056cb658cb9ca8051bab18e8deef9221353d6a8a
Dani1324 <danieleronchini2@gmail.com>
2023-02-12 20:09:16 +0100

    12

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ popup.addEventListener('mouseleave', function() {
 gsap.registerPlugin(ScrollTrigger);
 const myText = new SplitType('.line');
 const imageSliderPin = gsap.timeline();
+const andvantagesSliderPin = gsap.timeline();
+const zoom = gsap.timeline();
 
 
 // GSAP - text animation
@@ -84,6 +86,133 @@ imageSliderPin.eventCallback("onComplete", function() {
     y: "-0.5vw",
     opacity: 0,
     scale: 1.05,
+  });
+
+});
+
+
+// GSAP - advantages slider - pin
+andvantagesSliderPin. to(".advantages", {
+  scrollTrigger: {
+    trigger: ".advantages",
+    start: "95% 85%",
+    end: "100% -0%",
+    pin: true,
+    toggleActions: "repeat none none none",
+    markers: false,
+  },
+})
+
+andvantagesSliderPin.eventCallback("onComplete", function() {
+
+  // GSAP - advantages-slider - animation
+  const t2 = gsap.timeline({
+    ease: Power1.easeInOut,
+    scrollTrigger: {
+      trigger: ".horizontal-slider, #changing-text h2",
+      start: "95% 35%",
+      end: "90% -50%",
+      scrub: 3,
+      toggleActions: "repeat none none none",
+      markers: false,
+    },
+  });
+
+  const t3 = gsap.timeline({
+    ease: Power1.easeInOut,
+    scrollTrigger: {
+      trigger: ".horizontal-slider, #changing-text h2",
+      start: "95% 35%",
+      end: "90% -50%",
+      scrub: 3,
+      toggleActions: "repeat none none none",
+      markers: false,
+    },
+  });
+
+  t2.to(".horizontal-slider", {
+    duration: 30,
+    x: "-128.8vw",
+  });
+
+  t3.to("#changing-text h2", {
+    duration: 30,
+    y: "-4.32vw",
+  });
+
+  t2.to(".horizontal-slider", {
+    duration: 60,
+    x: "-299.1vw",
+  });
+
+  t3.to("#changing-text h2", {
+    duration: 60,
+    y: "-8.6vw",
+  });
+
+  t2.to(".horizontal-slider", {
+    duration: 30,
+    x: "-384.6vw",
+  });
+
+  t3.to("#changing-text h2", {
+    duration: 30,
+    y: "-12.95vw",
+  });
+
+  t2.to(".horizontal-slider", {
+    duration: 30,
+    x: "-427.5vw",
+  });
+
+  t3.to("#changing-text h2", {
+    duration: 30,
+    y: "-17.3vw",
+  });
+
+  t2.to(".horizontal-slider", {
+    duration: 30,
+    x: "-470vw",
+  });
+
+  t3.to("#changing-text h2", {
+    duration: 30,
+    y: "-21.6vw",
+  });
+
+});
+
+
+// GSAP - zoom - pin
+zoom. to(".experience", {
+  scrollTrigger: {
+    trigger: ".experience",
+    start: "100% 100%",
+    end: "100% 0%",
+    pin: true,
+    toggleActions: "repeat none none none",
+    markers: false,
+  },
+})
+
+zoom.eventCallback("onComplete", function() {
+
+  // GSAP - zoom - animation
+  const zoom = gsap.timeline({
+    ease: Power3.easeInOut,
+    scrollTrigger: {
+      trigger: ".experience h2",
+      start: "50% 80%",
+      end: "100% 20%",
+      scrub: 3,
+      toggleActions: "repeat none none none",
+      markers: false,
+    },
+  });
+
+  zoom.from(".experience h2", {
+    scale: 0.5,
+    y: "8vw",
   });
 
 });
