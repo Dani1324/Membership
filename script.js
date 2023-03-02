5e2d751acdd95efabac0ff9548baf55271ab6755
Dani1324 <danieleronchini2@gmail.com>
2023-03-02 22:12:57 +0100

    25

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ var carousel = document.querySelector('.carousel-inner');
 
 setInterval(function() {
   carousel.classList.add('animate');
-  /*setTimeout(function() {
-    carousel.classList.remove('animate');
-  }, 15000);*/
 }, 0);
 
+
+// Image slider gsap animation
+const t1 = gsap.timeline();
 
 // Value animation
 const c1 = document.getElementsByClassName("c-1");
@@ -29,6 +29,17 @@ const c3 = document.getElementsByClassName("c-3");
 
 document.getElementById("value-btn-1").classList.add('selected-value');
 document.getElementById("c1").classList.add('selected-carousel');
+document.getElementById("c1").classList.add('animate1');
+
+t1.to(".animate1", {
+    x: "-33vw",
+    scrollTrigger: {
+      trigger: ".value__carousel",
+      start: "20% 60%",
+      end: "20% 20%",
+      scrub: 1,
+    },
+});
 
 function value1(n) {
   // Button
@@ -44,6 +55,53 @@ function value1(n) {
   document.getElementById("c3").classList.remove('selected-carousel');
   document.getElementById("c4").classList.remove('selected-carousel');
   document.getElementById(`c${n}`).classList.add('selected-carousel');
+
+  // Image carousel
+  document.getElementById("c1").classList.remove('animate1');
+  document.getElementById("c2").classList.remove('animate2');
+  document.getElementById("c3").classList.remove('animate3');
+  document.getElementById("c4").classList.remove('animate4');
+  document.getElementById(`c${n}`).classList.add(`animate${n}`);
+
+  t1.to(".animate1", {
+    x: "-33vw",
+    scrollTrigger: {
+      trigger: ".value__carousel",
+      start: "20% 60%",
+      end: "20% 20%",
+      scrub: 1,
+    },
+  });
+
+  t1.to(".animate2", {
+    x: "-70.5vw",
+    scrollTrigger: {
+      trigger: ".value__carousel",
+      start: "20% 60%",
+      end: "20% 20%",
+      scrub: 2,
+    },
+  });
+
+  t1.to(".animate3", {
+    x: "2.5vw",
+    scrollTrigger: {
+      trigger: ".value__carousel",
+      start: "20% 60%",
+      end: "20% 20%",
+      scrub: 1,
+    },
+  });
+
+  t1.to(".animate4", {
+    x: "-33vw",
+    scrollTrigger: {
+      trigger: ".value__carousel",
+      start: "20% 60%",
+      end: "20% 20%",
+      scrub: 1,
+    },
+  });
 };
 
 
